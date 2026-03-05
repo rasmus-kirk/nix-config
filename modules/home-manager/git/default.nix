058adf56df1935a363ddb53ac9ca3185ebfa45a7
rasmus-kirk <mail@rasmuskirk.com>
2026-03-05 19:51:55 +0100

    Signkey works

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -45,7 +45,7 @@ in {
         signByDefault = true;
       };
       settings = {
-        gpg.ssh.allowedSignersFile = "${cfg.signKey}";
+        # gpg.ssh.allowedSignersFile = "${cfg.signKey}";
         user.email = cfg.userEmail;
         user.name = cfg.userName;
         alias = {
