f7e690cfa406814bc2c830ba40f447bb4fe9d90b
rasmus-kirk <mail@rasmuskirk.com>
2026-03-10 15:30:52 +0100

    Changes

--- configurations/home-manager/ubuntu-container/home.nix
+++ configurations/home-manager/ubuntu-container/home.nix
@@ -13,9 +13,6 @@
 in {
   kirk = {
     terminalTools.enable = true;
-    # foot.enable = true;
-    # mpv.enable = true;
-    # mvi.enable = true;
     xdgMime.enable = true;
     git = {
       enable = true;
@@ -45,24 +42,10 @@ in {
       rootDir = dataDir;
       autoSortDownloads = true;
     };
-    # zathura = {
-    #   enable = true;
-    #   darkmode = false;
-    # };
     zsh = {
       enable = true;
       stateDir = stateDir;
     };
-    # fonts.enable = true;
-    # chromiumLaunchers = {
-    #   enable = true;
-    #   stateDir = stateDir;
-    #   launchers = {
-    #     gemini = "https://gemini.google.com/";
-    #     claude-website = "https://claude.ai/";
-    #     slack = "https://concordium.slack.com/";
-    #   };
-    # };
   };
 
   home.username = username;
@@ -75,45 +58,15 @@ in {
 
   targets.genericLinux.enable = true;
 
-  # services = {
-  #   podman.enable = true;
-  #   syncthing.enable = true;
-  # };
-
-  # systemd.user.tmpfiles.rules = [
-  #   "d  ${stateDir}/thunderbird     0755 user user - -"
-  #   "d  ${stateDir}/cosmic          0755 user user - -"
-  #   "d  ${stateDir}/cosmic/config   0755 user user - -"
-  #   "d  ${stateDir}/cosmic/comp     0755 user user - -"
-  #   "d  ${stateDir}/cosmic/local    0755 user user - -"
-  #   "d  ${stateDir}/firefox         0755 user user - -"
-  #   "d  ${stateDir}/firefox/config  0755 user user - -"
-  #   "d  ${stateDir}/firefox/home    0755 user user - -"
-  #   "d  ${stateDir}/chromium        0755 user user - -"
-  #   "d  ${stateDir}/syncthing       0755 user user - -"
-  #   "d  ${stateDir}/syncthing/state 0755 user user - -"
-  #   "d  ${stateDir}/syncthing/sync  0755 user user - -"
-
-  #   "L+ ${config.home.homeDirectory}/.thunderbird                  - - - - ${stateDir}/thunderbird"
-  #   "L+ ${config.home.homeDirectory}/.mozilla                      - - - - ${stateDir}/firefox/home"
-  #   "L+ ${config.home.homeDirectory}/.config/mozilla               - - - - ${stateDir}/firefox/config"
-  #   "L+ ${config.home.homeDirectory}/.config/chromium              - - - - ${stateDir}/chromium"
-  #   "L+ ${config.home.homeDirectory}/.local/state/syncthing        - - - - ${stateDir}/syncthing/state"
-
-  #   "L+ ${config.home.homeDirectory}/.config/cosmic             - - - - ${stateDir}/cosmic/config"
-  #   "L+ ${config.home.homeDirectory}/.local/state/cosmic        - - - - ${stateDir}/cosmic/local"
-  #   "L+ ${config.home.homeDirectory}/.local/state/cosmic-comp   - - - - ${stateDir}/cosmic/comp"
-  # ];
-
   programs.bash = {
     enable = true;
-    initExtra = ''
-      if [[ "$PWD" == "$HOME" ]]; then
-        cd /data
-      fi
+    # initExtra = ''
+    #   if [[ "$PWD" == "$HOME" ]]; then
+    #     cd /data
+    #   fi
 
-      exec zsh
-    '';
+    #   exec zsh
+    # '';
   };
 
   programs.zsh.profileExtra = ''
